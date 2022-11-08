bd6d1d6f3252f58c32dcd51f9d8ef69e4157f03c
Oferlis <Ofer.Lis@ocean.ibm.com>
2022-11-08 15:30:59 +0200

    WIP - API changes to facilitate filter

--- app/controllers/api/physical_storages_controller.rb
+++ app/controllers/api/physical_storages_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class PhysicalStoragesController < BaseController
+  class PhysicalStoragesController < BaseProviderController
     def refresh_resource(type, id, _data = nil)
       enqueue_ems_action(type, id, "Refreshing", :method_name => :refresh_ems)
     end
